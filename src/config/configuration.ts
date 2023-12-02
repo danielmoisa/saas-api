d219ba8979d9c41f2c229cf051287085d1853806
Daniel Moisa <danimoisa93@gmail.com>
2023-12-02 16:13:50 +0200

    add config module

---
+++ src/config/configuration.ts
@@ -0,0 +1,9 @@
+export default () => ({
+  port: parseInt(process.env.PORT ?? '8080', 10),
+  database: {
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT ?? '5432', 10),
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+  },
+});
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const configService = app.get<ConfigService>(ConfigService);
+
+  const port = configService.get('PORT');
+
+  await app.listen(port);
 }
 bootstrap();
