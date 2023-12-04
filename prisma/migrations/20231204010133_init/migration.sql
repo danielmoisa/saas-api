234c2791727db8664cedfdb0b008d43bd83879e4
Daniel Moisa <danimoisa93@gmail.com>
2023-12-04 03:01:49 +0200

    add initial migrations

---
+++ prisma/migrations/20231204010133_init/migration.sql
@@ -0,0 +1,174 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" BIGSERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "image" TEXT,
+    "emailVerified" BOOLEAN NOT NULL DEFAULT false,
+    "birthDate" DATE,
+    "registrationDate" TIMESTAMP(6) NOT NULL DEFAULT timezone('UTC'::text, now()),
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "workspaces" (
+    "id" BIGSERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "userId" BIGINT NOT NULL,
+
+    CONSTRAINT "workspaces_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "companies" (
+    "id" BIGSERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "description" TEXT,
+    "employeeRange" TEXT NOT NULL,
+    "linkedin" TEXT NOT NULL,
+    "twitter" TEXT NOT NULL,
+    "website" TEXT NOT NULL,
+    "phone" BIGINT,
+    "email" TEXT,
+    "workspaceId" BIGINT NOT NULL,
+
+    CONSTRAINT "companies_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "persons" (
+    "id" BIGSERIAL NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" BIGINT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "workspaceId" BIGINT NOT NULL,
+
+    CONSTRAINT "persons_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "tasks" (
+    "id" BIGSERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "priority" TEXT NOT NULL,
+    "workspaceId" BIGINT NOT NULL,
+
+    CONSTRAINT "tasks_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "flows" (
+    "id" BIGSERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "column4" BIGINT NOT NULL,
+    "workspaceId" BIGINT NOT NULL,
+
+    CONSTRAINT "flows_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "email-changes" (
+    "token" CHAR(21) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "newEmail" TEXT NOT NULL,
+    "userId" BIGINT NOT NULL,
+    "validUntil" TIMESTAMP(6) NOT NULL DEFAULT (timezone('utc'::text, now()) + '2 days'::interval),
+
+    CONSTRAINT "email-changes_pkey" PRIMARY KEY ("token")
+);
+
+-- CreateTable
+CREATE TABLE "email-verifications" (
+    "token" CHAR(21) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" BIGINT NOT NULL,
+    "validUntil" TIMESTAMP(6) NOT NULL DEFAULT (timezone('utc'::text, now()) + '2 days'::interval),
+
+    CONSTRAINT "email-verifications_pkey" PRIMARY KEY ("token")
+);
+
+-- CreateTable
+CREATE TABLE "password-resets" (
+    "token" CHAR(21) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" BIGINT NOT NULL,
+    "validUntil" TIMESTAMP(6) NOT NULL DEFAULT (timezone('utc'::text, now()) + '2 days'::interval),
+
+    CONSTRAINT "password-resets_pkey" PRIMARY KEY ("token")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "workspaces_userId_key" ON "workspaces"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "companies_workspaceId_key" ON "companies"("workspaceId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "persons_email_key" ON "persons"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "persons_workspaceId_key" ON "persons"("workspaceId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "tasks_workspaceId_key" ON "tasks"("workspaceId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "flows_workspaceId_key" ON "flows"("workspaceId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "email-changes_userId_key" ON "email-changes"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "email-verifications_userId_key" ON "email-verifications"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "password-resets_userId_key" ON "password-resets"("userId");
+
+-- AddForeignKey
+ALTER TABLE "workspaces" ADD CONSTRAINT "workspaces_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "companies" ADD CONSTRAINT "companies_workspaceId_fkey" FOREIGN KEY ("workspaceId") REFERENCES "workspaces"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "persons" ADD CONSTRAINT "persons_workspaceId_fkey" FOREIGN KEY ("workspaceId") REFERENCES "workspaces"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "tasks" ADD CONSTRAINT "tasks_workspaceId_fkey" FOREIGN KEY ("workspaceId") REFERENCES "workspaces"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "flows" ADD CONSTRAINT "flows_workspaceId_fkey" FOREIGN KEY ("workspaceId") REFERENCES "workspaces"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "email-changes" ADD CONSTRAINT "email-changes_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "email-verifications" ADD CONSTRAINT "email-verifications_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "password-resets" ADD CONSTRAINT "password-resets_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
